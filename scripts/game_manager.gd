58176a21aeff6f8ba1f5667a543dcd68ac4e67f3
Lyhdyr <claraesqtei@gmail.com>
2024-11-21 22:51:26 +0100

    obstacle random spawn position

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,20 +1,26 @@
 class_name GameManager extends Node2D
 
-var viewport_dim : Vector2 # This will store the viewport size
-var offset : float = 0 # An extra we add to the viewport size
+var viewport_area : Vector2 # This will store the viewport size
+var spawn_area : Vector2 # Area where obstacles will spawn
+var offset : float = 50.0 # An extra we add to the viewport size
 var time : int
 var stored_delta : float
 
 @onready var player: Player = %Player # A reference to the Player
 @onready var label: Label = $CanvasLayer/Label # Time label
 
-
 const OBSTACLE := preload("res://scenes/obstacle.tscn") # Preloads the Obstacle scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	viewport_dim = get_viewport_rect().size # Gets the viewport size (648 x 480)
+	viewport_area = get_viewport_rect().size # Gets the viewport size (648 x 480)
+	# It’s better to get it like this because 
+	# we might want change the viewport size in the future!
 
+	var max_area = Vector2(
+			viewport_area.x + offset, 
+			viewport_area.y + offset)
+	spawn_area = max_area - viewport_area
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,17 +35,10 @@ func _on_timer_timeout() -> void:
 	var obstacle := OBSTACLE.instantiate()
 	
 	# The obstacle spawns in a random position outside the viewport
-	# HARDCODED VERSION (NOT THE BEST)
-	#obstacle.position = Vector2(
-		#randf_range(-50, 698), 
-		#randf_range(-50, 530)) # The viewport is 648 * 480
-	
-	# BETTER VERSION
+	var random_vec2 := Vector2(randf_range(-1, 1), randf_range(-1, 1))
 	obstacle.position = Vector2(
-		randf_range(player.position.x - viewport_dim.x/2 - offset,  player.position.x + viewport_dim.x/2 + offset), 
-		randf_range(player.position.y - viewport_dim.y/2 - offset,  player.position.y + viewport_dim.y/2 + offset))
-	#print(str(obstacle.position))
-	
-	add_child(obstacle)
+			player.position.x + viewport_area.x/2 * sign(random_vec2.x) + spawn_area.x/2 * random_vec2.x,
+			player.position.y + viewport_area.y/2 * sign(random_vec2.y) + spawn_area.y/2 * random_vec2.y)
 	
 	obstacle.direction = obstacle.position.direction_to(player.position) # The obstacle moves towards the Player!
+	add_child(obstacle) # We add the new obstacle to the Game scene
--- scripts/obstacle.gd
+++ scripts/obstacle.gd
@@ -5,9 +5,9 @@ var speed : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	speed = randf_range(100, 300) #Random speed to add variety
+	speed = randf_range(100, 300) # Random speed to add variety
 
-# Calculate position based on speed, direction and delta time
+# Calculates position based on speed, direction and delta time
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 
